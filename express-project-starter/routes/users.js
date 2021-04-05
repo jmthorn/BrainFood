b19cd20aa379cf2b5b85e9f2955cfe731e6e4543
jonathan <jonathanchan.808@gmail.com>
2021-04-05 16:25:15 -0400

    Worked on utils file

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const {loginUser, logoutUser } = require('../auth');
+const { check, validationResult } = require('express-validator');
+const  db  = require('../db/models');
+
 
 /* GET users listing. */
+
+
+
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
