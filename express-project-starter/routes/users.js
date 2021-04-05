0270f8fa45450e612662cd46619c73c04e9a2561
Keith Mellea <keithmellea@gmail.com>
2021-04-05 16:46:28 -0400

    addding validator function

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const { csrfProtection, asyncHandler } = require('./utils');
 const {loginUser, logoutUser } = require('../auth');
 const { check, validationResult } = require('express-validator');
 const  db  = require('../db/models');
@@ -7,11 +10,39 @@ const  db  = require('../db/models');
 
 /* GET users listing. */
 
-
-
-
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/user/signup', csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render('user-signup', {
+    title: 'Sign Up',
+    user,
+    csrfToken: req.csrfToken(),
+  });
 });
 
+const userValidators = [
+  check('username')
+   .exists({ checkFalsy: true})
+   .withMessage('Please provide a value for Username')
+   .isLength({ max: 20 })
+   .withMessage('Username must not be more than 20 characters'),
+  check('email')
+   .exists({ checkFalsy: true })
+   .withMessage('Please provide a value for email')
+   .isLength({ max: 255 })
+   .withMessage('Email must not be more than 255 characters')
+   .isEmail()
+   .withMessage('Provided email is not a valid email')
+   .custom((value) => {
+     return db.User.findOne({ where: { emailAddress: value }})
+      .then((user) => {
+        if (user) {
+          return Promise.reject('The provided email is already in use by another account');
+        }
+      });
+   })
+]
+
+router.post('/user/signup', csrfProtection, asyncHandler(async (req, res) => {
+
+}))
 module.exports = router;
