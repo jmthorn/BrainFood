6011eef41e4baa0f9dcfcb7a35f8a19d54fb52b0
jonathan <jonathanchan.808@gmail.com>
2021-04-06 19:29:57 -0400

    Worked on navbar.css, bookshelf css and bookshelf routes

---
+++ express-project-starter/routes/bookshelf.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const { csrfProtection, asyncHandler } = require('./utils');
+const { loginUser, logoutUser } = require('../auth');
+const { check, validationResult } = require('express-validator');
+const db = require('../db/models');
+
+
+// const bookValidators = [
+//   check('title')
+//     .exists({checkFalsy: true})
+//     .withMessage('Please provide a value for title')
+//     .isLength({ max: 255})
+//     .withMessage()
+// ]
+
+
+/* GET bookshelves. */
+
+router.get('/bookshelves/:id', asyncHandler(async (req, res) => {
+  const bookshelfId = parseInt(req.params.bookshelfId, 10);
+
+  const books = await db.Book.findAll({
+    where: {
+      bookshelfId
+    },
+    include: Bookshelf
+  });
+
+  res.render('bookshelf', {
+    books
+  })
+
+}))
+
+
+findAll({include: Book},{where: ID})
