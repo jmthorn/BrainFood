7d51cb757b628b3d134d506a0ecc488795a10b3b
Skulllady <noorhoque45@gmail.com>
2021-04-07 14:55:09 -0400

    working on adding description to book page and on our eit functionality

--- express-project-starter/db/models/review.js
+++ express-project-starter/db/models/review.js
@@ -3,13 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const Review = sequelize.define('Review', {
     author: DataTypes.STRING,
     rating: DataTypes.INTEGER,
+    review: DataTypes.TEXT,
     userId: DataTypes.INTEGER,
     bookId: DataTypes.INTEGER
   }, {});
   Review.associate = function (models) {
     // associations can be defined here
-    Review.belongsTo(models.Book, { foreignKey: "bookId"})
-    Review.belongsTo(models.User, { foreignKey: "userId"})
+    Review.belongsTo(models.Book, { foreignKey: "bookId" })
+    Review.belongsTo(models.User, { foreignKey: "userId" })
   };
   return Review;
 };
--- express-project-starter/routes/books.js
+++ express-project-starter/routes/books.js
@@ -7,20 +7,30 @@ const { loginUser, logoutUser } = require('../auth');
 const { check, validationResult } = require('express-validator');
 const db = require('../db/models');
 
-router.get("/:id", asyncHandler(async(req, res) => { 
+router.get("/:id", asyncHandler(async (req, res) => {
     let bookId = parseInt(req.params.id, 10)
     let book = await db.Book.findByPk(bookId)
-    res.render('book', { 
-        book
+    let reviews = await db.Review.findAll(
+        {
+            where: { bookId }
+        }
+    )
+    res.render('book', {
+        book,
+        reviews
     })
 }))
 
 
-router.put("/:id", asyncHandler(async(req,res) => { 
+router.put("/:id", asyncHandler(async (req, res) => {
     let bookId = parseInt(req.params.id, 10)
-    let book = await db.Book.findByPk(bookId)
-    const { cover, title, author, published} = req.body
-    await book.update({ cover, title, author, published});
+    // let book = await db.Book.findByPk(bookId)
+    const { cover, title, author, published, description } = req.body
+    // await book.update({ cover, title, author, published, description });
+    let book = await db.Book.update({ cover, title, author, published, description }, {
+        where: { bookId }
+    })
+    res.redirect(`books/${bookId}`)
 }))
 
 
