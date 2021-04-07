656216e8063486af5943708ac8d66c0c4e8c33da
jonathan <jonathanchan.808@gmail.com>
2021-04-07 17:02:25 -0400

    Added profile.js and profile.pug files

---
+++ express-project-starter/routes/profile.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const { csrfProtection, asyncHandler } = require('./utils');
+const { loginUser, logoutUser } = require('../auth');
+const { check, validationResult } = require('express-validator');
+const db = require('../db/models');
+
+
+router.get('/', csrfProtection, asyncHandler(async(req, res) => {
+  res.render('profile')
+}))
