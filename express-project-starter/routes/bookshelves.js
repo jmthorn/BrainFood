31e06aa4c2a7a3000cd4f7ccb1a340a8b48cfdf1
Keith Mellea <keithmellea@gmail.com>
2021-04-07 09:13:33 -0400

    working on add books to bookshelf page

--- express-project-starter/routes/bookshelves.js
+++ express-project-starter/routes/bookshelves.js
@@ -37,6 +37,55 @@ router.get('/', asyncHandler(async (req, res) => {
   })
 })) 
 
+// Delete specific Bookshelf Route
+router.delete("/:id", asyncHandler(async (req, res) => {
+    const bookshelfId = parseInt(req.params.id, 10);
+    const bookshelf = await db.Bookshelf.findByPk(bookshelfId);
+    const books = await db.Book.findAll();
+    // const bookshelfId = parseInt(req.params.bookshelfId, 10);
+    // const books = await db.Book.findAll({
+    //   where: {
+    //     bookshelfId,
+    //   },
+    //   include: db.Bookshelf,
+    // });
+    await bookshelf.destroy();
+
+    res.redirect(`bookshelf/${bookshelfId - 1}`, {
+      bookshelves,
+      books,
+    });
+  })
+);
+
+router.get('/add-book', asyncHandler(async (req, res) => {
+  const book = db.Book.build();
+  res.render('add-book-to-bookshelf', {
+    book,
+  });
+}))
+
+// Delete specific Book Route
+router.delete("/:id", asyncHandler(async (req, res) => {
+    // How do we do this?
+    const bookId = parseInt(req.params.id, 10);
+    const book = await db.Book.findByPk(bookId);
+    //const books = await db.Book.findAll();
+    // const bookshelfId = parseInt(req.params.bookshelfId, 10);
+    // const books = await db.Book.findAll({
+    //   where: {
+    //     bookshelfId,
+    //   },
+    //   include: db.Bookshelf,
+    // });
+    await book.destroy();
+
+    res.render("bookshelf", {
+      bookshelves,
+      books,
+    });
+  })
+);
 // router.get('/bookshelves/:id', asyncHandler(async (req, res) => {
 //   const bookshelfId = parseInt(req.params.bookshelfId, 10);
 //   // const books = await db.Book.findAll({
