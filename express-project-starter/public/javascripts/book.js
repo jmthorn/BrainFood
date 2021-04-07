3fbf7b9e3697da73a993467f4add9e5f4120ebe4
Skulllady <noorhoque45@gmail.com>
2021-04-07 19:15:59 -0400

    nothing new added just troubleshooting

--- express-project-starter/public/javascripts/book.js
+++ express-project-starter/public/javascripts/book.js
@@ -2,33 +2,34 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
     document.getElementById("submit-review")
-        .addEventListener("click", event => { 
+        .addEventListener("click", event => {
             event.preventDefault();
             let bookId = event.target.baseURI.split('/')[4]
             let textarea = document.querySelector(".new-review")
             let review = textarea.value
             let rating = document.getElementById('book-rating').value
-            fetch(`http://localhost:8080/books/${bookId}/reviews`, { 
+            console.log("book ID", bookId)
+            fetch(`http://localhost:8080/books/${bookId}/reviews`, {
                 method: "POST",
-                headers: { 
+                headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                review, 
-                bookId, 
-                rating
+                    review,
+                    bookId,
+                    rating
                 })
             })
-                .then(function(res) { 
+                .then(function (res) {
                     return res.json()
                 })
-                .then(function(data) { 
-                    console.log(data)
-                })
-                .catch(function(error) {
-                console.log(error);
-                });
-            
+            // .then(function(data) {
+            //     console.log(data)
+            // })
+            // .catch(function(error) {
+            // console.log(error);
+            // });
+
         })
 
 
