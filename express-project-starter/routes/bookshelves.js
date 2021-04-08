bd9faa8f1a072ffcdd1a1aef2e5a00db2961da82
Jessica T <jmthorn16@gmail.com>
2021-04-08 10:16:08 -0500

    add bookshelves

--- express-project-starter/routes/bookshelves.js
+++ express-project-starter/routes/bookshelves.js
@@ -23,7 +23,7 @@ const db = require('../db/models');
 router.get('/', asyncHandler(async (req, res) => {
   // const bookshelfId = parseInt(req.params.id, 10);
   // const bookshelves = await db.Bookshelf.findAll();
-  // const books = await db.Book.findAll();
+  const books = await db.Book.findAll();
   // // const bookshelfId = parseInt(req.params.bookshelfId, 10);
   // const books = await db.Bookshelf.findbyPk(bookshelfId, {
   //   include: { model: db.Book,
