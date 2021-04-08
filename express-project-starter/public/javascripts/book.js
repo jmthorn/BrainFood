0cbf45fe4368c501a7c467cc8e7f7d4d3a70891e
Jessica T <jmthorn16@gmail.com>
2021-04-07 21:31:01 -0500

    added modal for edit books

--- express-project-starter/public/javascripts/book.js
+++ express-project-starter/public/javascripts/book.js
@@ -24,7 +24,19 @@ window.addEventListener("DOMContentLoaded", () => {
             
         })
 
+        let modal = document.getElementById("modal")
+        let editBtn = document.querySelector(".book-edit")
+        
+        editBtn.addEventListener("click", (event) => { 
+            modal.classList.remove("hidden")
+            modal.classList.add("modal-show")
+        })
 
-
+        window.addEventListener("click", (event) => {
+            if (event.target == modal) {
+                modal.classList.remove("modal-show")
+                modal.classList.add("hidden")
+            }
+        })
 
 })
