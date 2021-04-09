70523ea18e671341d24a9b0d391e72c88222fd0d
Keith Mellea <keithmellea@gmail.com>
2021-04-09 16:37:24 -0400

    images on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
-const quotes = require("./quotes.js")
+const quotes = require("./quotes.js");
+const { asyncHandler } = require('./utils.js');
+const db = require("../db/models");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Brainfood' });
-});
+router.get('/', asyncHandler( async (req, res, next) => {
+  const tagId = parseInt(req.params.id, 10); 
+  const books = await db.Book.findAll({
+      include: db.Tag,
+  });
+  res.render('index', { title: 'Brainfood',
+books });
+}));
 
-module.exports = router;
 
 router.get("/quote", function (req, res) {
   const randomNumber = Math.floor(Math.random() * quotes.length)
   res.json(quotes[randomNumber])
 });
+
+router.get('/', asyncHandler(async (req, res) => {
+
+    
+
+}))
+
+
+module.exports = router;
